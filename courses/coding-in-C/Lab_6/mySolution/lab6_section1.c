8313180fb4a1370d9161e15d3146e1141b011ddb
Sefa <yilmaz.sefa-it25@it.dhbw-ravensburg.de>
2026-02-18 13:58:21 +0100

    Bearbeitung der Aufgabe

--- courses/coding-in-C/Lab_6/mySolution/lab6_section1.c
+++ courses/coding-in-C/Lab_6/mySolution/lab6_section1.c
@@ -1,2 +1,27 @@
+#include <stdio.h>
 
-alo = 2;
+
+int main(void) {
+
+    FILE *f = fopen("input.txt", "r");
+    //FILE *f2 = fopen("test.txt", "w");
+    
+    if (f == NULL) {
+        printf("Fehler beim Öffnen der Datei.\n");
+        return 1;
+    }
+    /*int c = fgetc(f);
+    printf("Dateiinhalt 1 Zeichen: %c\n", c);
+    char s[100];
+    fgets(s, 100, f);
+    printf("Dateiinhalt 1 Zeile: %s\n", s);
+    fclose(f);
+    return 0;
+    */
+   char p[100];
+    fscanf(f, "%s", p);
+    printf("Dateiinhalt 1 Wort: %s\n", p);
+    fclose(f);  
+
+    return 0;
+}
