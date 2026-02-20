ecab697d8405cce7dc48fadab85e550d28d1b4aa
Sefa <yilmaz.sefa-it25@it.dhbw-ravensburg.de>
2026-02-20 13:34:45 +0100

    Lab6_Section2 fertiggestellt

---
+++ courses/coding-in-C/Lab_6/mySolution/car_fuel.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+struct car {
+    float fuel_level;
+    float max_fuel_level;
+    char model[50];
+};
+
+void refuel(struct car *car, float amount)
+{
+    if (car->fuel_level + amount <= car->max_fuel_level) 
+    {
+        car->fuel_level += amount;
+    } 
+    else 
+    {
+        car->fuel_level = car->max_fuel_level;
+    }
+}
+
+int main(void)
+{
+    struct car myCar;
+
+    myCar.fuel_level = 10.0;
+    myCar.max_fuel_level = 50.0;
+    strcpy(myCar.model, "Toyota Corolla");
+
+    printf("Before refueling:\n");
+    printf("Model: %s\n", myCar.model);
+    printf("Fuel level: %.2f / %.2f\n\n", myCar.fuel_level, myCar.max_fuel_level);
+
+    refuel(&myCar,35.0);
+
+    printf("After refueling:\n");
+    printf("Model: %s\n", myCar.model);
+    printf("Fuel level: %.2f / %.2f\n", myCar.fuel_level, myCar.max_fuel_level);
+
+    return 0;
+}
