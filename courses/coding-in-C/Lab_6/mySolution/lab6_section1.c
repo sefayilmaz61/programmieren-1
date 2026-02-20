a600b9786d3a97d692a8ab1a3729f2b69f74c7ef
Sefa <yilmaz.sefa-it25@it.dhbw-ravensburg.de>
2026-02-20 13:52:17 +0100

    Lab 6 - Section 1: fertiggestellt

--- courses/coding-in-C/Lab_6/mySolution/lab6_section1.c
+++ courses/coding-in-C/Lab_6/mySolution/lab6_section1.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
 
 int main(void) {
+   
+    FILE *f = fopen("C:/Users/sefaa/programmieren1/courses/coding-in-C/Lab_6/input.txt", "r");
 
-    FILE *f = fopen("input.txt", "r");
-    //FILE *f2 = fopen("test.txt", "w");
     
-    if (f == NULL) {
+    if (f == NULL) 
+    {
         printf("Fehler beim Öffnen der Datei.\n");
         return 1;
     }
-    /*int c = fgetc(f);
+
+    int c = fgetc(f);
     printf("Dateiinhalt 1 Zeichen: %c\n", c);
+
     char s[100];
     fgets(s, 100, f);
-    printf("Dateiinhalt 1 Zeile: %s\n", s);
-    fclose(f);
-    return 0;
-    */
-   char p[100];
+    printf("Dateiinhalt 1 Zeile: %s", s);
+    
+    char p[100];
     fscanf(f, "%s", p);
     printf("Dateiinhalt 1 Wort: %s\n", p);
+
+    FILE *f2 = fopen("C:/Users/sefaa/programmieren1/courses/coding-in-C/Lab_6/input.txt", "w");
+
+    if (f2 == NULL) 
+    {
+        printf("Fehler beim Öffnen der Datei.\n");
+        return 1;
+    }
+    
+    
+    fputc('Y', f2);
+    fputs("Hallo Welt!\n", f2);
+    fprintf(f2, "Trabzon61 kara\n");
     fclose(f);  
 
-    return 0;
+    
+
+    fclose(f2);
+
+
 }
